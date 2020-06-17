6b308e4ceb557032ef889207a22fcf7747f5dce7
Gebruiker <Gebruiker@DESKTOP-2FMMJQ7>
2020-06-17 20:36:48 +0200

    Opruiming

--- src/H12qz1/Superklasse.java
+++
@@ -1,6 +0,0 @@
-package H12qz1;
-
-public class Superklasse
-{
-  protected int x = 5; //Wordt aangeroepen vanuit de subklasse.
-}
--- src/_12_overerving/qz01/Superklasse.java
+++
@@ -1,6 +0,0 @@
-package _12_overerving.qz01;
-
-public class Superklasse
-{
-  protected int x = 5;
-}
